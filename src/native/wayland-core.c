0745d027365deb7cff5b09b1ad41febeba1de34d
cainb <cainbryce208@gmail.com>
2025-08-13 17:02:05 -0600

    feat(native): keep hello N-API symbol and export wl_get_api entrypoint

--- src/native/wayland-core.c
+++ src/native/wayland-core.c
@@ -1,3 +1,6 @@
+#include <node/js_native_api.h>
+#include <node/js_native_api_types.h>
+#include <node/node_api.h>
 #include <stdint.h>
 
 typedef int (*wl_add_fn)(int, int);
@@ -18,4 +21,10 @@ static const struct wl_api WL_API_INSTANCE = {
 
 __attribute__((visibility("default"))) const struct wl_api* wl_get_api(void) {
     return &WL_API_INSTANCE;
+}
+
+napi_value hello(napi_env env) {
+    napi_value result;
+    napi_create_object(env, &result);
+    return result;
 }
