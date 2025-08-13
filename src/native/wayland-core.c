04c8a704c9967fbf7331acb7fc8312298366efd7
cainb <cainbryce208@gmail.com>
2025-08-13 17:01:26 -0600

    feat(native): export wl_get_api entrypoint returning function table with versioned ABI

--- src/native/wayland-core.c
+++ src/native/wayland-core.c
@@ -1,9 +1,21 @@
-#include <node/js_native_api.h>
-#include <node/js_native_api_types.h>
-#include <node/node_api.h>
+#include <stdint.h>
 
-napi_value hello(napi_env env) {
-    napi_value result;
-    napi_create_object(env,&result);
-    return result;
+typedef int (*wl_add_fn)(int, int);
+
+struct wl_api {
+    int32_t version;
+    wl_add_fn add;
+};
+
+static int wl_add_impl(int a, int b) {
+    return a + b;
+}
+
+static const struct wl_api WL_API_INSTANCE = {
+    1,
+    wl_add_impl,
+};
+
+__attribute__((visibility("default"))) const struct wl_api* wl_get_api(void) {
+    return &WL_API_INSTANCE;
 }
